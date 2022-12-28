6115144a091ac62730138b0b5f41331be8c9036a
ngocdat <huynhngocdat2002.td@gmail.com>
2022-12-28 15:10:01 +0700

    convert usePaing.js to ts

--- src/hooks/usePaging.ts
+++ src/hooks/usePaging.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 export const usePaging = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [pageIndex, setPageIndex] = useState(1);
-  const handleNextPage = (totalPage) => {
+  const handleNextPage = (totalPage: number) => {
     if (totalPage > pageIndex) setPageIndex((prev) => prev + 1);
     if (pageIndex === totalPage) return;
   };
@@ -10,7 +10,7 @@ export const usePaging = () => {
     if (pageIndex === 1) return;
     setPageIndex((prev) => prev - 1);
   };
-  const handleItemPaging = (index) => {
+  const handleItemPaging = (index: number) => {
     setPageIndex(index);
   };
   return { pageIndex, handleNextPage, handlePrevPage, handleItemPaging };
